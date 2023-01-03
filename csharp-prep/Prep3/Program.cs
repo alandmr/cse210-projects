b8f5f6573ed0d44d064ae4fddbdd90cc1c33581e
alandmr <fenix_3006@hotmail.com>
2023-01-03 09:04:26 -0400

    Assignment C#/Prep3 finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,58 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {        
+        Random randomGenerator = new Random();
+        int magic_number = 0;
+        int number;
+        string getNumber;
+        bool isNumeric;
+        string answer = "";
+        int trackingGuesses;
+
+        answer = "yes";
+        while (answer.ToLower() == "yes")
+        {
+            magic_number = randomGenerator.Next(1,100);
+            number = 0;
+            getNumber = "";
+            isNumeric = false;
+            trackingGuesses = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                getNumber = Console.ReadLine();
+                isNumeric = int.TryParse(getNumber, out number);
+                if (isNumeric)
+                {
+                    trackingGuesses++;
+                    if (number < magic_number)
+                    {
+                        Console.WriteLine("Higher");  
+                    }
+                    else if (number > magic_number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("You guessed it!");
+                        Console.WriteLine($"Tracking guesses: {trackingGuesses}");
+                        Console.WriteLine("");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("");
+                }
+            }while(magic_number != number);    
+
+            Console.WriteLine("Do you want to play again? ");
+            answer = Console.ReadLine();        
+        }
     }
 }
