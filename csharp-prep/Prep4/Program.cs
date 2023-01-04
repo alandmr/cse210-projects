89b419fd4ec016bc2dfcb25097ec093ecaee73ff
alandmr <fenix_3006@hotmail.com>
2023-01-04 11:01:21 -0400

    Assignment C#/Prep4 finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> list = new List<int>();
+        int number = -1;        
+        bool isNumeric;
+        string getNumber = "";
+               
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            getNumber = Console.ReadLine();
+            isNumeric = int.TryParse(getNumber, out number);
+            if (isNumeric)
+            {
+                if (number != 0)
+                {
+                    list.Add(number);
+                }                
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Invalid input");
+                Console.WriteLine("");
+            }
+        }
+       
+        
+        Console.WriteLine($"The sum is: {list.Sum()}");
+        Console.WriteLine($"The average is: {list.Average()}");
+        Console.WriteLine($"The largest number is: {list.Max()}");
+        list.Sort();
+                
+        List<int> positiveNumList = new List<int>();
+        foreach (int n in list)
+        {
+            if (n > 0)
+            {
+                positiveNumList.Add(n);
+            }
+        }
+        
+        Console.WriteLine($"The smallest positive number is: {positiveNumList.Min()}");        
+        Console.WriteLine("The sorted list is:");
+        foreach (int n in list)
+        {
+            Console.WriteLine(n);
+        }
+
     }
 }
