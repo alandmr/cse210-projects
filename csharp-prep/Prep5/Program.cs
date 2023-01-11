54e381129bb2512979d44dff6ef403888b09196f
alandmr <fenix_3006@hotmail.com>
2023-01-11 10:40:11 -0400

    Assignment Prep5/C# finished

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string userName = "";
+        int scuare = 0;
+        int number = 0;
+
+        DisplayWelcome();
+        userName = PromptUserName();
+        number = PromptUserNumber();
+        scuare = SquareNumber(number);
+        DisplayResult(userName,scuare);
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        string name = "";
+
+        Console.Write("Please enter your name: ");
+        name = Console.ReadLine();        
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        int num = 0;
+        
+        Console.Write("Please enter your favorite number: ");
+        num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
+
 }
