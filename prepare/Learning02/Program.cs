77ce2b233d44f477379846a021caacd49407c189
alandmr <fenix_3006@hotmail.com>
2023-01-24 06:13:19 -0400

    Assignment Lerning02 Finished

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engeneer";
+        job1._company = "Microsoft";
+        job1._startYear = 2009;
+        job1._endYear = 2011;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2015;
+        job2._endYear = 2017;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Aland Munoz";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);  
+
+        myResume.Display();      
+
+        
     }
 }
